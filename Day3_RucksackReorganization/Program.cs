b25a93ccc34e199941d83ca5c55428a3f9c923a5
igrilkul <igrilkul@hotmail.com>
2022-12-06 22:52:58 +0200

    day 3 and 4 done

---
+++ Day3_RucksackReorganization/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day3_RucksackReorganization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = File.ReadAllText(@"C:\Users\Isuskata\source\repos\AdventCalendar22\ConsoleApp1\Day3_RucksackReorganization\input.txt");
+            var rucksacks = input.Split("\r\n").ToArray();
+
+            var result = 0;
+
+            for(int i = 0; i < rucksacks.Count() ; i++)
+            {
+                var rucksackA = rucksacks[i];
+                var rucksackB = rucksacks[i+1];
+                var rucksackC = rucksacks[i+2];
+
+                var commonItemsA = rucksackA.Intersect(rucksackB);
+                var commonItemsB = rucksackB.Intersect(rucksackC);
+
+                var commonItem = commonItemsA.Intersect(commonItemsB).First();
+
+                int itemValue = (int)commonItem;
+
+                if(itemValue >= 97 && itemValue <= 122)
+                {
+                    itemValue -= 96;
+                }
+                else
+                {
+                    itemValue -= 38;
+                }
+
+                result += itemValue;
+                //Console.WriteLine(rucksack);
+                //Console.WriteLine(first);
+                //Console.WriteLine(last);
+                //Console.WriteLine(commonItem);
+                //Console.WriteLine(itemValue);
+
+                i += 2;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Day4_CampCleanup/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day4_CampCleanup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(@"C:\Users\Isuskata\source\repos\AdventCalendar22\ConsoleApp1\Day4_CampCleanup\input.txt");
+
+            var pairs = input.Split("\r\n").Select(p => p.Split(',')).ToArray();
+
+            int counter = 0;
+            foreach (var pair in pairs)
+            {
+                var elfA = pair[0];
+                var elfB = pair[1];
+
+                int elfAStart = Int32.Parse(elfA.Split('-')[0]);
+                int elfBStart = Int32.Parse(elfB.Split('-')[0]);
+
+                int elfAEnd = Int32.Parse(elfA.Split('-')[1]);
+                int elfBEnd = Int32.Parse(elfB.Split('-')[1]);
+
+                bool isContained = false;
+
+                for(int i= elfAStart; i <= elfAEnd; i++)
+                {
+                    for(int j= elfBStart; j <= elfBEnd; j++)
+                    {
+                        if(i == j)
+                        {
+                            counter++;
+                            isContained = true;
+                            break;
+                        }
+                    }
+
+                    if (isContained)
+                    {
+                        break;
+                    }
+                }
+                
+
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
